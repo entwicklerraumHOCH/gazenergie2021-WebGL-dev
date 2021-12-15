5d1009d06573bf052fc769f819b2c45ce09058a3
Filip <czekajlofilip@gmail.com>
2021-12-15 14:02:05 +0100

    dfa312f70a7d2e4bca0e2d01c7da75454c114085

---
+++ TemplateData/src/VideoViewer.js
@@ -0,0 +1,40 @@
+﻿var videoPlayerContainer = document.getElementById("video-player-container");
+var videoCloseButton = document.getElementById("video-close-button");
+var videoPlayer = document.getElementById("video-player");
+var videoSource = document.getElementById("video-source");
+var videoTitle = document.getElementById("video-title");
+var videoSourceText = document.getElementById("video-source-text");
+var videoOverlay = document.getElementById("video-overlay");
+videoOverlay.style.opacity = 0;
+
+videoPlayer.addEventListener("mouseenter", showOverlay);
+videoPlayer.addEventListener("mouseleave", hideOverlay);
+videoCloseButton.addEventListener("click", onCloseClicked);
+
+function showOverlay()
+{
+    videoOverlay.style.opacity = 1;
+}
+
+function hideOverlay()
+{
+    videoOverlay.style.opacity = 0;
+}
+
+function onCloseClicked()
+{
+    videoPlayer.removeAttribute("src");
+    videoPlayerContainer.style.display = "none";
+}
+
+function showVideo(url, title, source)
+{
+    videoPlayerContainer.style.display = "block";
+    videoPlayer.setAttribute("src", url);
+    videoTitle.innerText = title;
+    videoSourceText.innerText = source;
+
+    videoPlayer.pause();
+    videoPlayer.currentTime = 0;
+    videoPlayer.load();
+}
